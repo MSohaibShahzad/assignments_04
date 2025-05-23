348079c81fb37f12c63f176317234c8a759d2330
MSohaibShahzad <160708298+MSohaibShahzad@users.noreply.github.com>
2025-05-23 18:41:28 +0500

    Add files via upload

--- assignments 1 to 6/madlibs/madlibs.py
+++ assignments 1 to 6/madlibs/madlibs.py
@@ -1,1 +1,31 @@
 
+
+
+
+
+
+
+
+
+
+#  concaatenation
+
+food : str = "Biryani"
+
+print("He loves to eat " + food)
+print("She cooks {} very nice".format(food))
+print(f"All of them enjoyed the {food} a lot")
+
+
+# creating a sentence 
+adj : str = input("Enter an adjective: ")
+verb1 : str = input("Enter a verb: ")
+verb2 : str = input("Enter one more verb: ")
+famous : str = input("Enter any famous person: ")
+
+madlibs : str = f"Computer programing is so {adj}! It makes me so excited all the time because \
+I love to {verb1}, Stay hydrated and {verb2} like you are {famous}!"
+
+print(madlibs)
+
+
